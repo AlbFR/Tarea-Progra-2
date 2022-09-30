d3fdad0c56624025e8d2685041f6b799f32d9ee2
AlbFR <albertoferrada20@gmail.com>
2022-09-30 13:52:09 -0300

    OrdenCompra.java created

---
+++ Tarea-Progra-2/src/tarea/progra/pkg2/OrdenCompra.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class OrdenCompra {
+    private Date fecha;
+    private Srting estado;
+    public OrdenCompra () {}
+    public float calcPrecioSinIVA () {}
+    public float calcIVA () {}
+    public float calcPrecio () {}
+    public float calcPeso () {}
+}
--- Tarea-Progra-2/src/tarea/progra/pkg2/TareaProgra2.java
+++ Tarea-Progra-2/src/tarea/progra/pkg2/TareaProgra2.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tarea.progra.pkg2;
 
-/**
- *
- * @author albfr
- */
 public class TareaProgra2 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World :D");
     }
-    
 }
